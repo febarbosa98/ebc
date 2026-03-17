8126d9d761401ed9c60fad0d2aab4a4fa4a17eab
Fernando Barbosa <fernando_fe1@hotmail.com>
2026-03-17 18:06:57 -0300

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,45 @@
-import type { Metadata, Viewport } from 'next'
-import { Inter, Playfair_Display } from 'next/font/google'
-// import { Analytics } from '@vercel/analytics/next'
+import type { Metadata } from 'next'
 import './globals.css'
 
-const _inter = Inter({ subsets: ["latin"] });
-const _playfair = Playfair_Display({ subsets: ["latin"], variable: "--font-serif" });
+
+
+// export const metadata: Metadata = {
+//   title: 'EBC Serviços Terceirizados',
+//   description: 'Empresa de segurança especializada em proteção empresarial, segurança de eventos e segurança pessoal. Profissionais qualificados e tecnologia de ponta.',
+//   keywords: ['segurança', 'segurança empresarial', 'segurança de eventos', 'segurança pessoal', 'EBC'],
+// }
 
 export const metadata: Metadata = {
-  title: 'EBC Serviços Terceirizados | Segurança Profissional',
-  description: 'Empresa de segurança especializada em proteção empresarial, segurança de eventos e segurança pessoal. Profissionais qualificados e tecnologia de ponta.',
-  keywords: ['segurança', 'segurança empresarial', 'segurança de eventos', 'segurança pessoal', 'EBC'],
+  title: {
+    default: 'EBC Serviços Terceirizados',
+    template: '%s | EBC Serviços Terceirizados',
+  },
+
+  description:
+    'Soluções em controle de acesso, portaria, recepção, apoio operacional para obras e segurança patrimonial em São Paulo.',
+
+  openGraph: {
+    title: 'EBC Serviços Terceirizados',
+    description:
+      'Serviços terceirizados com qualidade, segurança e profissionais treinados.',
+    url: 'https://ebcservicos.com',
+    siteName: 'EBC Serviços Terceirizados',
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  keywords: [
+    'serviços terceirizados',
+    'controle de acesso',
+    'portaria terceirizada',
+    'recepção empresarial',
+    'apoio operacional para obras',
+    'segurança patrimonial',
+    'empresa de segurança em São Paulo',
+    'serviços terceirizados SP',
+    'controle de acesso São Paulo',
+    'portaria para condomínios',
+    'EBC Serviços Terceirizados'
+  ],
 }
 
 export default function RootLayout({
